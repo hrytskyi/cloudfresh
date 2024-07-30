e51869daea45c24dd4e106fca87fecab2f4d70d6
Hrytskyi Danylo <d.hrytskyi@gmail.com>
2024-07-30 21:07:33 +0300

    Working solution for enabling ci/cd pipelines and code tests

--- app/models.py
+++ app/models.py
@@ -11,7 +11,17 @@ def get_board(board_id):
     return app.db.boards.find_one({"_id": ObjectId(board_id)})
 
 def add_task(board_id, task_name, task_status='Backlog'):
+    task = {"_id": ObjectId(), "name": task_name, "status": task_status}
     app.db.boards.update_one(
         {"_id": ObjectId(board_id)},
-        {"$push": {"tasks": {"name": task_name, "status": task_status}}}
+        {"$push": {"tasks": task}}
+    )
+
+def delete_board(board_id):
+    app.db.boards.delete_one({"_id": ObjectId(board_id)})
+
+def delete_task(board_id, task_id):
+    app.db.boards.update_one(
+        {"_id": ObjectId(board_id)},
+        {"$pull": {"tasks": {"_id": ObjectId(task_id)}}}
     )
